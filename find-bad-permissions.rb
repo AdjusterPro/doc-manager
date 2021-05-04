fd36e5fd7887552a222ecb8b25582ef2fa6ee773
Ben Dunlap <ben@adjusterpro.com>
2021-05-04 15:02:32 -0500

    find-bad-permissions - gracefully exit with output if we catch an unexpected exception //bdunlap

--- find-bad-permissions.rb
+++ find-bad-permissions.rb
@@ -35,19 +35,24 @@ agent = GoogleBackup.new(
 )
 
 i = 0
-output = agent.g.files.map do |f|
-  obj = { :id => f.id, :title => f.title }
-  begin
-    obj[:bad_permissions] = f.acl
-    .reject { |a| email_ok?(a.email_address) }
-    .map { |a| pretty_up(a) }
-  rescue Google::Apis::ClientError => e
-    raise e unless e.message =~ /insufficientFilePermissions/
-    obj[:bad_permissions] = "[unauthorized]"
+begin
+  output = []
+  agent.g.files do |f|
+    obj = { :id => f.id, :title => f.title, :type => f.mime_type }
+    begin
+      obj[:bad_permissions] = f.acl
+      .reject { |a| email_ok?(a.email_address) }
+      .map { |a| pretty_up(a) }
+    rescue Google::Apis::ClientError => e
+      raise e unless e.message =~ /insufficientFilePermissions/
+      obj[:bad_permissions] = "[unauthorized]"
+    end
+    i += 1
+    logger.info("processed #{i} files") if i % 25 == 0
+    output << obj
   end
-  i += 1
-  logger.info("processed #{i} files") if i % 25 == 0
-  obj
+rescue Exception => e
+  logger.error("Ended prematurely with #{e.class}: #{e.message}")
 end
 
 puts JSON.pretty_generate(output)
